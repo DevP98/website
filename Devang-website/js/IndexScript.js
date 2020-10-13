f47a57fa4c82af97ee2585a5a8763b96d6982e32
Devang <devang.pandey98@gmail.com>
2020-10-13 18:22:01 -0400

    all changes
    Signed-off-by: Devang <devang.pandey98@gmail.com>

--- Devang-website/js/IndexScript.js
+++ Devang-website/js/IndexScript.js
@@ -6,8 +6,8 @@ window.onload = (function(){
     var b = 0;
     var titleText = 'Hello!';
     var descriptionText = "My name is Devang Pandey and I am a Bachelor of Science in Statistics with a double minor in Economics and International Development Studies from the University of Toronto!";
-    var speedSlow = 75;
-    var speedFast = 20;
+    var speedSlow = 200;
+    var speedFast = 50;
     typeWriter();
     function typeWriter() {
         if (i < titleText.length) {
